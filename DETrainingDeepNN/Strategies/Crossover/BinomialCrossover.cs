1e48f79fdef9fc9b4d1d2ca3d02f28f80770d297
Kristina Georgieva <kristina.s.georgieva@gmail.com>
2016-06-08 22:08:35 +0200

    Added crossover

---
+++ DETrainingDeepNN/Strategies/Crossover/BinomialCrossover.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DETrainingDeepNN.Strategies.Crossover
+{
+    public class BinomialCrossover
+    {
+        private double probability;
+        private Random random;
+
+        public BinomialCrossover(Random random)
+        {
+            this.probability = Double.Parse(ConfigurationManager.AppSettings["CrossoverProbability"]);
+            this.random = random;
+        }
+        
+        public BinomialCrossover(Random random, double probability)
+        {
+            this.probability = probability;
+            this.random = random;
+        }
+
+        public Individual Cross(Individual individual1, Individual individual2)
+        {
+            int length = individual1.Position.Length;
+
+            double[] newPosition = new double[length];
+            
+            for (int index = 0; index < length; index++)
+            {
+                newPosition[index] = random.NextDouble() > probability 
+                    ? individual1.Position[index] 
+                    : individual2.Position[index];
+            }
+
+            return new Individual
+            {
+                Position = newPosition
+            };
+        }
+        
+    }
+}
---
+++ Test_DETrainingDeepNN/Strategies/Crossover/BinomialCrossoverTest.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DETrainingDeepNN;
+using DETrainingDeepNN.Strategies.Crossover;
+using System.Linq;
+
+namespace Test_DETrainingDeepNN.Strategies.Crossover
+{
+    [TestClass]
+    public class BinomialCrossoverTest
+    {
+        [TestMethod]
+        public void GivenAnTwoIndividuals_WhenTheyAreCombined_ItShouldReturnAnIndividualDifferentFromTheInputs()
+        {
+
+            Individual individual1 = new Individual
+            {
+                Position = new double[] { 1.0, 2.0, 3.0 }
+            };
+
+            Individual individual2 = new Individual
+            {
+                Position = new double[] { 9.0, 11.0, 11.0 }
+            };
+
+            BinomialCrossover crossoverStrategy = new BinomialCrossover(new Random(2));
+
+            Individual result = crossoverStrategy.Cross(individual1, individual2);
+
+            Assert.IsFalse(individual1.Position.SequenceEqual(result.Position));
+            Assert.IsFalse(individual2.Position.SequenceEqual(result.Position));
+        }
+
+        [TestMethod]
+        public void GivenAnTwoIndividuals_WhenTheyAreCombined_ItShouldReturnAnIndividualWithPartsFromEachIndividual()
+        {
+
+            Individual individual1 = new Individual
+            {
+                Position = new double[] { 1.0, 2.0, 3.0 }
+            };
+
+            Individual individual2 = new Individual
+            {
+                Position = new double[] { 9.0, 11.0, 11.0 }
+            };
+
+            BinomialCrossover crossoverStrategy = new BinomialCrossover(new Random(2));
+
+            Individual result = crossoverStrategy.Cross(individual1, individual2);
+
+            int totalFromIndividual1 = 0;
+            int totalFromIndividual2 = 0;
+
+            for (int i = 0; i < individual1.Position.Length; i++)
+            {
+                if(result.Position[i] == individual1.Position[i])
+                {
+                    totalFromIndividual1++;
+                } else
+                {
+                    totalFromIndividual2++;
+                }
+            }
+
+            Assert.IsTrue(totalFromIndividual1 > 0);
+            Assert.IsTrue(totalFromIndividual2 > 0);
+        }
+
+        [TestMethod]
+        public void GivenAprobabilityOfZero_WhenIndividualsAreCrossed_ItShouldReturnAnIndividualWithThePositionOfIndividualOne()
+        {
+
+            Individual individual1 = new Individual
+            {
+                Position = new double[] { 1.0, 2.0, 3.0 }
+            };
+
+            Individual individual2 = new Individual
+            {
+                Position = new double[] { 9.0, 11.0, 11.0 }
+            };
+
+            BinomialCrossover crossoverStrategy = new BinomialCrossover(new Random(5), 0.0);
+
+            Individual result = crossoverStrategy.Cross(individual1, individual2);
+
+            Assert.IsTrue(individual1.Position.SequenceEqual(result.Position));
+        }
+
+        [TestMethod]
+        public void GivenAprobabilityOfOne_WhenIndividualsAreCrossed_ItShouldReturnAnIndividualWithThePositionOfIndividualOne()
+        {
+
+            Individual individual1 = new Individual
+            {
+                Position = new double[] { 1.0, 2.0, 3.0 }
+            };
+
+            Individual individual2 = new Individual
+            {
+                Position = new double[] { 9.0, 11.0, 11.0 }
+            };
+
+            BinomialCrossover crossoverStrategy = new BinomialCrossover(new Random(5),1.0);
+
+            Individual result = crossoverStrategy.Cross(individual1, individual2);
+
+            Assert.IsTrue(individual2.Position.SequenceEqual(result.Position));
+        }
+
+    }
+}
