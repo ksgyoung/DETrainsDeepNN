b972a230374422beb209be968bafbf9c842274b6
Kristina Georgieva <kristina.s.georgieva@gmail.com>
2016-06-11 11:22:42 +0200

    Added rastrigin fitness strategy to check standard DE works

---
+++ DETrainingDeepNN/Strategies/FitnessEvaluation/RastriginFitnessEvaluationStrategy.cs
@@ -0,0 +1,20 @@
+﻿using DETrainingDeepNN.Strategies.FitnessEvaluation.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DETrainingDeepNN.Strategies.FitnessEvaluation
+{
+    public class RastriginFitnessEvaluationStrategy : IRastriginFitnessEvaluationStrategy
+    {
+        public double GetFitnessForIndividual(Individual individual)
+        {
+            int dimensions = individual.Position.Length;
+            double[] positions = individual.Position;
+
+            return 10 * dimensions + positions.Sum(x => Math.Pow(x, 2) - 10 * Math.Cos(2 * Math.PI * x));
+        }
+    }
+}
---
+++ Test_DETrainingDeepNN/Strategies/FitnessEvaluation/RastriginFitnessEvaluationStrategyTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DETrainingDeepNN.Strategies.FitnessEvaluation;
+using DETrainingDeepNN;
+
+namespace Test_DETrainingDeepNN.Strategies.FitnessEvaluation
+{
+    [TestClass]
+    public class RastriginFitnessEvaluationStrategyTest
+    {
+        [TestMethod]
+        public void GivenAnIndividual_WhenTheFitnessIsCalculatedUsingTheRastriginFunction_ItShouldReturnTheExpectedValue()
+        {
+            RastriginFitnessEvaluationStrategy rastriginCalculator = new RastriginFitnessEvaluationStrategy();
+
+            Individual individual = new Individual
+            {
+                Position = new double[] { 1.0, 2.0, 3.0 }
+            };
+
+            double value = rastriginCalculator.GetFitnessForIndividual(individual);
+
+            Assert.AreEqual(14, value);
+        }
+    }
+}
