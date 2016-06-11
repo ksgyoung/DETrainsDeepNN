9fa2f4c151556351bd110bf5996b18f26e540fcc
Kristina Georgieva <kristina.s.georgieva@gmail.com>
2016-06-11 10:16:35 +0200

    Added minimisation elitist selection strategy

---
+++ DETrainingDeepNN/Strategies/Selection/MinimisationElitistSelection.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DETrainingDeepNN.Strategies.Selection
+{
+    public class MinimisationElitistSelection
+    {
+        public Individual Select(List<Individual> individuals)
+        {
+            Individual mostFitIndividual = individuals[0];
+
+            foreach (Individual indivividual in individuals)
+            {
+                if (indivividual.Fitness < mostFitIndividual.Fitness)
+                {
+                    mostFitIndividual = indivividual;
+                }
+            }
+            
+            return mostFitIndividual;
+        }
+    }
+}
---
+++ Test_DETrainingDeepNN/Strategies/Selection/MinimisationElitistSelectionTest.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using DETrainingDeepNN.Strategies.Selection;
+using DETrainingDeepNN;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Test_DETrainingDeepNN.Strategies.Selection
+{
+    [TestClass]
+    public class MinimisationElitistSelectionTest
+    {
+        [TestMethod]
+        public void GivenTwoIndividualsWithDifferingFitnesses_WhenTheMinimisationElitistSelectionIsUsed_ItShouldReturnTheIndividualWithTheLowerFitness()
+        {
+            Individual individual1 = new Individual
+            {
+                Fitness = 2.5555
+            };
+
+            Individual individual2 = new Individual
+            {
+                Fitness = 1.234
+            };
+            
+            MinimisationElitistSelection selection = new MinimisationElitistSelection();
+            Individual result = selection.Select(new List<Individual>
+            {
+                individual1, individual2
+            });
+
+            Assert.AreEqual(individual2, result);
+        }
+
+        [TestMethod]
+        public void GivenThreeIndividualsWithDifferingFitnesses_WhenTheMinimisationElitistSelectionIsUsed_ItShouldReturnTheIndividualWithTheLowerFitness()
+        {
+            Individual individual1 = new Individual
+            {
+                Fitness = 2.5555
+            };
+
+            Individual individual2 = new Individual
+            {
+                Fitness = 1.234
+            };
+
+            Individual individual3 = new Individual
+            {
+                Fitness = 1.444
+            };
+
+            MinimisationElitistSelection selection = new MinimisationElitistSelection();
+            Individual result = selection.Select(new List<Individual>
+            {
+                individual1, individual2, individual3
+            });
+
+            Assert.AreEqual(individual2, result);
+        }
+
+        [TestMethod]
+        public void GivenTwoIndividualsWithTheSameFitnesses_WhenTheMinimisationElitistSelectionIsUsed_ItShouldReturnTheFirtsOfTheTwoIndividuals()
+        {
+            Individual individual1 = new Individual
+            {
+                Fitness = 1.234
+            };
+
+            Individual individual2 = new Individual
+            {
+                Fitness = 1.234
+            };
+            
+            MinimisationElitistSelection selection = new MinimisationElitistSelection();
+            Individual result = selection.Select(new List<Individual>
+            {
+                individual1, individual2
+            });
+
+            Assert.AreEqual(individual1, result);
+        }
+    }
+}
